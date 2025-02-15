aa5c7e246b8d2102b95d03f95472a2206a887c03
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2025-02-15 18:36:54 +0300

    About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { Product }  from '../models/product.model'
 
 @Component({
   selector: 'app-about',
@@ -9,5 +12,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  product: Product | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    const productId = Number(this.route.snapshot.paramMap.get('id'));
+    this.product = this.productService.getProductById(productId);
+  }
 }
